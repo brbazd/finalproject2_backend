f95a1dc935cc3a8567bcdbd225e1fbf52c41e4a2
brbazd <branko.bazdar@gmail.com>
2023-04-30 20:50:14 +0200

    v2

--- views/login.view.php
+++ views/login.view.php
@@ -2,6 +2,7 @@
 
 
 <div class="container">
+    <h3>Login</h3>
     <form action="login" method="post">
         <div>
             <label for="email">Email</label>
@@ -15,6 +16,32 @@
         <button type="submit">Login</button>
     </form>
 </div>
+
+
+<style>
+
+    h3 {
+        text-align: center;
+        margin: 0;
+        margin-top: 24px;
+        font-size: 40px;
+    }
+
+    form {
+        width: 500px;
+        height: 500px;
+        margin: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    form div {
+        padding: 20px;
+    }
+
+</style>
 
 
 <?php require "partials/footer.php" ?>
